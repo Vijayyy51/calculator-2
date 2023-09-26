19d30a3299b5b96217c12506b8094df9b7cc5774
Vijay kumar <97590491+Vijayyy51@users.noreply.github.com>
2023-09-27 02:37:49 +0530

    Add files via upload

---
+++ calcu.js
@@ -0,0 +1,29 @@
+let ans = "";
+let buttons = document.querySelectorAll(".button")
+
+Array.from(buttons).forEach((button) =>{
+    button.addEventListener("click", (e)=>{
+
+        if(e.target.innerHTML == "="){
+            ans = eval(ans);
+            document.querySelector("input").value = ans ;
+
+        }
+        else if(e.target.innerHTML == "AC"){
+            ans = "";
+            document.querySelector("input").value = ans ;
+
+        }
+        else if(e.target.innerHTML == "del"){
+            ans = ans.slice(0,-1);
+            document.querySelector("input").value = ans ;
+
+        }
+        else{
+            
+            ans = ans + e.target.innerHTML;
+            console.log(ans);
+            document.querySelector("input").value = ans ;
+        }
+    })
+})
